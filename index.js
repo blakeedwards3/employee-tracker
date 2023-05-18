d6cfb96d6f8561f317dfd9d966e430ddc2a11f21
Blake Edwards <blakeedwards.nc@gmail.com>
2023-05-18 17:51:01 -0400

    Started functions for the prompt in index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'company_db'
+});
+
+function startApp() {
+    inquirer.prompt([
+        {
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ],
+        },
+    ])
+        .then((answer) => {
+            if (answer.action === 'View all departments') {
+                viewAllDepartments()
+            } else if (answer.action === 'View all employees') {
+                viewAllEmployees()
+            } else if (answer.action === 'View all Roles') {
+                viewAllRoles()
+            } else if (answer.action === 'Add new employee') {
+                addNewEmployee()
+            } else if (answer.action === 'Add new department') {
+                addNewDepartment()
+            } else if (answer.action === 'Add new role') {
+                addNewRole()
+            } else if (answer.action === 'Update employee role') {
+                updateEmployeeRole()
+            } else {
+                connection.end()
+            }
+        })
+}
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+    startApp();
+})
